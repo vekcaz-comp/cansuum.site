655cde8d965d0ceaf649c0ca825f7dd72a55bcb1
vekcaz-comp <mcanaksoy06@gmail.com>
2025-04-24 21:04:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 window.onload = () => {
-const heart = document.getElementById('heart');
-const splashText = document.getElementById('splashText');
-const music = document.getElementById('bg-music');
-const footer = document.getElementById('footer');
+  const heart = document.getElementById('heart');
+  const splashText = document.getElementById('splashText');
+  const music = document.getElementById('bg-music');
+  const footer = document.getElementById('footer');
 
-heart.addEventListener('click', () => {
-  // Kalbi gizle
-  heart.style.display = 'none';
+  heart.addEventListener('click', () => {
+    // Kalbi gizle
+    heart.style.display = 'none';
 
-  // Yazıyı göster
-  splashText.style.display = 'block';
+    // Yazıyı göster
+    splashText.style.display = 'block';
 
-  // Müzik başlasın
-  music.muted = false; // mute aç
-  music.play(); // müziği başlat
-// Footer'ı göster
-footer.style.display = 'block';
-});
+    // Müzik başlasın
+    music.muted = false; // mute aç
+    music.play(); // müziği başlat
 
-
+    // Footer'ı göster
+    footer.style.display = 'block';
+  });
+};
